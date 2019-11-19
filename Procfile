cf1565393b55c78a72bc918325f58605c7047acc
Bobstin <justinakahn@gmail.com>
2019-11-19 13:02:03 -0500

    Adds celery to procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn django_vue.wsgi
+celery: celery -A django_vue worker --loglevel info --without-heartbeat -Q default --hostname default-%h
---
+++ example_app/tasks.py
@@ -0,0 +1,12 @@
+from django_vue.celery import app
+from datetime import datetime
+
+
+@app.task(queue="default")
+def example_task(message):
+    print(f'I am inside a celery task. The message I received was: {message}')
+
+
+@app.task(queue="default")
+def example_beat_task():
+    print(f'I am inside a celery task being called by celery beat. The the current time is {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
